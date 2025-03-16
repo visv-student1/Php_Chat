4ddf7dadcea4be2a70d640b9201f21759b81325a
visv-student1 <ffviswa6@gmail.com>
2025-03-16 14:37:07 +0530

    h

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
      session_start();
      if(isset($_SESSION['unique_id'])){
         header("location: users.php");
+        header("location: index.html");
      }
 ?>
 
 <?php include_once "./header.php"; ?>
-<?php include_once "./index.html"; ?>
+
 
